ddc9e9caae74e266af65d212cfd375add7f62584
Ezra Mabatu <ezramabatu@gmail.com>
2026-02-18 09:28:14 +0300

    Add oat_hash function for string hashing

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -206,6 +206,22 @@ int main(void)
   /* USER CODE END 3 */
 }
 
+uint32_t oat_hash(const char *s, size_t len){
+    unsigned char *p = (unsigned char*) s;
+    uint32_t h = 0;
+
+    while(len--) {
+        h += *p++;
+        h += (h << 10);
+        h ^= (h >> 6);
+    }
+
+    h += (h << 3);
+    h ^= (h >> 11);
+    h += (h << 15);
+
+    return h;
+}
 /**
   * @brief System Clock Configuration
   * @retval None
